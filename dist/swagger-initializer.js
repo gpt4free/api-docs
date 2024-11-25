2364cd2245dcb800efe47110a3e3ada38ab34d6e
H Lohaus <hlohaus@users.noreply.github.com>
2024-11-25 02:05:42 +0100

    Update swagger-initializer.js

--- dist/swagger-initializer.js
+++ dist/swagger-initializer.js
@@ -3,7 +3,7 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "https://raw.githubusercontent.com/hlohaus/g4f-swagger/refs/heads/master/dist/openapi.json",
+    url: "dist/openapi.json",
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
